399f79455b81801fbd430aebd45bf4b10b585e40
Mikael K. <mike.amanfo12@gmail.com>
2024-12-21 02:20:44 -0500

    Deployment Fixes

--- app/page.tsx
+++ app/page.tsx
@@ -3,7 +3,6 @@ import Articles from '@/components/articles/Articles';
 import Contact from '@/components/contact/Contact';
 import Hero from '@/components/hero/Hero';
 import ProjectsSection from '@/components/projects/ProjectsSection';
-import Reviews from '@/components/review/Reviews';
 import Techs from '@/components/techs/Techs';
 
 export default function Home() {
